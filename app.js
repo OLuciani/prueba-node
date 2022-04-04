7464b04c3b625a2a3978154bd38a9fc86135598c
OLuciani <lucianioscar1.gamil.com>
2022-04-04 20:49:17 -0300

    Cree la constante suma en el archivo app.js

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ const calculadora = require("./MODULOS/calculadora");
 let {sumar, restar} = calculadora;
 //const restar = require("./MODULOS/restar");
 
-console.log(sumar(6, 3));
+const suma = console.log(sumar(6, 3));
 console.log(restar(15, 4));
+
